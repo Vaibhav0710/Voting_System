c35c410b5fbbbc78ce217547bcdead4dbd538d13
Vaibhav_Jain <vaibhavjain7171@gmamil.com>
2025-04-15 20:01:15 +0530

    eureka-service added

---
+++ eureka-service/src/test/java/com/voting/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
